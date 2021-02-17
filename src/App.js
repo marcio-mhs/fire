c17515155ed9ef905ff8e4b2189e856004ccc8c3
marcio <marciomhs@hotmail.com>
2021-02-16 21:48:51 -0300

    aula 61

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {};
+
+    let firebaseConfig = {
+      apiKey: "AIzaSyB_nBUnQ0vj91hbB71VmeBpI5PPm7U5Iu0",
+      authDomain: "reactapp-bb79f.firebaseapp.com",
+      projectId: "reactapp-bb79f",
+      storageBucket: "reactapp-bb79f.appspot.com",
+      messagingSenderId: "578201414826",
+      appId: "1:578201414826:web:e3b85dc3ecc0c663d8b4d3"
+    };
+
+    if(!firebase.apps.length){
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    }
+    
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Funcionando!</h1>
+      </div>
+    );
+  }
+
 }
-
-export default App;
