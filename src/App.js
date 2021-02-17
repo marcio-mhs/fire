e67db9b8d2c24571e073bae35ad051b91f9684af
marcio-mhs <marciomhs@hotmail.com>
2021-02-17 09:05:27 -0300

    aula 63

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      tokenInput: '',
       token: 'Carregando ',
+      nomeInput: '',
       nome: '',
+      idadeInput: '',
       idade: '',
     };
+
+    this.cadastrar = this.cadastrar.bind(this);
 
     let firebaseConfig = {
       apiKey: "AIzaSyB_nBUnQ0vj91hbB71VmeBpI5PPm7U5Iu0",
@@ -24,35 +29,55 @@ export default class App extends Component{
       firebase.initializeApp(firebaseConfig);
     }
 
-    /*
     firebase.database().ref('token').on('value', (snapshot) => {
       let state = this.state;
       state.token = snapshot.val();
       this.setState(state);
-    });
-    */
-   firebase.database().ref('token').once('value').then((snapshot) => {
-    let state = this.state;
-    state.token = snapshot.val();
-    this.setState(state);
-   });
+    });    
 
-   firebase.database().ref('usuarios').child(1).on('value', (snapshot) => {
-    let state = this.state;
-    state.nome = snapshot.val().nome;
-    state.idade = snapshot.val().idade;
-    this.setState(state);
-  });
+    firebase.database().ref('usuarios').child(1).on('value', (snapshot) => {
+      let state = this.state;
+      state.nome = snapshot.val().nome;
+      state.idade = snapshot.val().idade;
+      this.setState(state);
+    });
     
+  }
+
+  cadastrar(e){
+    //firebase.database().ref('token').set(this.state.tokenInput);
+    //firebase.database().ref('usuarios').child(1).child('idade').set(this.state.tokenInput);
+    //firebase.database().ref('usuarios').child(1).child('cargo').set(this.state.tokenInput);
+    //firebase.database().ref('usuarios').child(1).child('cargo').remove();
+
+    let usuarios = firebase.database().ref('usuarios');
+    let chave = usuarios.push().key;
+    usuarios.child(chave).set({
+      nome: this.state.nomeInput,
+      idade: this.state.idadeInput,
+    });
+    e.preventDefault();
   }
 
   render(){
     const {token, nome, idade} = this.state;
     return(
       <div>
+
+        <form  onSubmit={this.cadastrar}>
+          <div>
+            <input type="text" value={this.state.nomeInput} onChange={(e) => this.setState({nomeInput: e.target.value})} />
+            <br/>
+            <input type="text" value={this.state.idadeInput} onChange={(e) => this.setState({idadeInput: e.target.value})} />
+            <br/>
+            <button type="submit">Cadastrar</button>
+          </div>  
+        </form>        
+
         <h1>Token: {token}</h1>
         <h1>Nome: {nome}</h1>
         <h1>Idade: {idade}</h1>
+
       </div>
     );
   }
