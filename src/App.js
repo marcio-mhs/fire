e431e5c8f3dd8fd9dbb941f24bdb283694bdc330
marcio <marciomhs@hotmail.com>
2021-02-17 20:30:00 -0300

    aula 68

--- src/App.js
+++ src/App.js
@@ -2,62 +2,86 @@ import React, {Component} from 'react';
 import firebase from './fireConnection';
 
 export default class App extends Component{
-  
+
   constructor(props){
     super(props);
     this.state = {
-      nome: '',
       email: '',
-      senha: ''
+      senha: '',
+      user:null
     };
-
     this.cadastrar = this.cadastrar.bind(this);
+    this.logar = this.logar.bind(this);
+    this.auth = this.auth.bind(this);
+    this.sair = this.sair.bind(this);
 
-    firebase.auth().signOut();
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user){
-        firebase.database().ref('usuarios').child(user.uid).set({
-          nome: this.state.nome
-        })
-        .then(() => {
-          this.setState({nome: '', email: '', senha: ''});
-        });
+  }
+
+  componentDidMount(){
+    this.auth();
+  }
+
+  auth(){
+    firebase.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({user: user});
       }
     })
-    
   }
 
-  cadastrar(e){
-   
+  cadastrar(){
+
     firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.senha)
     .catch((error) =>{
-        alert('Códido do erro: ' + error.code);
+        alert('Codigo de error: ' + error.code);
     });
-
-    e.preventDefault();
-
+  
   }
 
+  logar(){
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.senha)
+    .catch((error) =>{
+        alert('Codigo de error: ' + error.code);
+    });
+  }
+
+  sair(){
+    firebase.auth().signOut()
+    .then(()=>{
+      this.setState({user: null, email: '', senha:''});
+    })
+    ;
+  }
+
+
   render(){
+
     return(
-      <div>
-        <h1>Novo Usuário</h1>
-        <form onSubmit={(e) => {this.cadastrar(e)}}>
+      <div> 
+          {this.state.user ?
           <div>
-            <label>Nome</label><br/>
-            <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})} />
-            <br/>
-            <label>E-mail</label><br/>
-            <input type="text" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />
-            <br/>
-            <label>Senha</label><br/>
-            <input type="password" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} />
-            <br/><br/>
-            <button type="submit">Cadastrar</button>
+            <p>Painel Admin</p>
+            <p>Seja bem vindo :)</p>
+            <p>{this.state.user.email}</p>
+            <p>{this.state.user.uid}</p>
+            <button onClick={this.sair}>Sair</button>
           </div> 
-        </form>
+          :
+          <div>
+            <h1>Seja bem vindo</h1>
+            <label>Email:</label><br/>
+            <input type="text" value={this.state.email} 
+                  onChange={(e) => this.setState({email: e.target.value})}/><br/>
+
+            <label>Senha:</label><br/>
+            <input type="text" value={this.state.senha} 
+                  onChange={(e) => this.setState({senha: e.target.value})}/><br/>
+
+            <button onClick={this.cadastrar}>Cadastrar</button>                 
+            <button onClick={this.logar}>Logar</button>                 
+          </div>
+          }
       </div>
     );
   }
-
 }
